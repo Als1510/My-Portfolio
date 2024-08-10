cf33f951a9a1f62c53669ec425c064c5c4f19723
Anshul Sharma <anshul.sharma@alectio.com>
2024-08-10 17:54:02 +0530

    fix: Add progress roadmap

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-if(navToggle) {
-  navToggle.addEventListener('click', ()=>{
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
     navMenu.classList.add('show-menu')
   })
 }
-if(navClose) {
-  navClose.addEventListener('click', ()=>{
+if (navClose) {
+  navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu')
   })
 }
@@ -15,18 +15,18 @@ if(navClose) {
 const navLink = document.querySelectorAll('.nav_link')
 function linkAction() {
   const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show-menu')  
+  navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 const skillsContent = document.getElementsByClassName('skills_content'),
-      skillsHeader = document.querySelectorAll('.skills_header')
+  skillsHeader = document.querySelectorAll('.skills_header')
 function toggleSkills() {
   let itemClass = this.parentNode.className
-  for(i = 0; i < skillsContent.length; i++) {
+  for (i = 0; i < skillsContent.length; i++) {
     skillsContent[i].className = 'skills_content skills_close'
   }
-  if(itemClass === 'skills_content skills_close') {
+  if (itemClass === 'skills_content skills_close') {
     this.parentNode.className = 'skills_content skills_open'
   }
 }
@@ -35,7 +35,7 @@ skillsHeader.forEach((el) => {
 })
 
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+  tabContents = document.querySelectorAll('[data-content]')
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.target)
@@ -43,7 +43,7 @@ tabs.forEach(tab => {
       tabContent.classList.remove('qualification_active')
     })
     target.classList.add('qualification_active')
-    tabs.forEach(tab =>{
+    tabs.forEach(tab => {
       tab.classList.remove('qualification_active')
     })
     tab.classList.add('qualification_active')
@@ -51,9 +51,9 @@ tabs.forEach(tab => {
 })
 
 const modalViews = document.querySelectorAll('.services_modal'),
-      modalBtns = document.querySelectorAll('.services_button'),
-      modalCloses = document.querySelectorAll('.services_modal-close')
-let modal = function(modalClick) {
+  modalBtns = document.querySelectorAll('.services_button'),
+  modalCloses = document.querySelectorAll('.services_modal-close')
+let modal = function (modalClick) {
   modalViews[modalClick].classList.add('active-modal')
 }
 modalBtns.forEach((modalBtn, i) => {
@@ -78,7 +78,7 @@ let swiper = new Swiper('.portfolio_container', {
   },
   pagination: {
     el: '.swiper-pagination',
-    clickable:true
+    clickable: true
   },
 })
 
@@ -90,7 +90,7 @@ function scrollActive() {
     const sectionHeight = current.offsetHeight
     const sectionTop = current.offsetTop - 50
     sectionId = current.getAttribute('id')
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
     } else {
       document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
@@ -101,14 +101,14 @@ window.addEventListener('scroll', scrollActive)
 
 function scrollHeader() {
   const nav = document.getElementById('header')
-  if(this.scrollY >= 80) nav.classList.add('scroll-header');
+  if (this.scrollY >= 80) nav.classList.add('scroll-header');
   else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 function scrollUp() {
   const scrollUp = document.getElementById('scroll-up');
-  if(this.scrollY >= 500) scrollUp.classList.add('show-scroll');
+  if (this.scrollY >= 500) scrollUp.classList.add('show-scroll');
   else scrollUp.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollUp)
@@ -118,15 +118,23 @@ const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
+const roadMapCard = document.getElementById('about_img')
+
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
-if(selectedTheme) {
+const updateRoadmapImage = () => {
+  const theme = getCurrentTheme();
+  roadMapCard.src = `https://roadmap.sh/card/tall/66b753b9b64402e0524ed413?variant=${theme}`
+}
+if (selectedTheme) {
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   document.body.classList[selectedTheme === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+  updateRoadmapImage();
 }
-themeButton.addEventListener('click', ()=>{
+themeButton.addEventListener('click', () => {
   document.body.classList.toggle(darkTheme)
   themeButton.classList.toggle(iconTheme)
   localStorage.setItem('selected-theme', getCurrentTheme())
   localStorage.setItem('selected-icon', getCurrentIcon())
+  updateRoadmapImage();
 })
